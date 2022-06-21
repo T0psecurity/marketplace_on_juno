8597cd347ab3342aaaa925fdd84a931063889e20
phillipscheryl <phillipscheryl615@gmail.com>
2022-06-22 04:33:17 +0800

    removed desktop version string

--- hope-marketplace/src/components/Footer/index.tsx
+++ hope-marketplace/src/components/Footer/index.tsx
@@ -49,7 +49,7 @@ const Footer: React.FC = () => {
           alt=""
           onClick={() => history.push("/")}
         />
-        <SubContent>Actually Desktop Only 🖥️</SubContent>
+        {/* <SubContent>Actually Desktop Only 🖥️</SubContent> */}
       </FooterInfo>
       <FooterInfo>
         <MainContent>
