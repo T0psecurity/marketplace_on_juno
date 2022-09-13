abd449c0f07635bf3039e8b6fde9f707e9ba0e06
phillipscheryl <phillipscheryl615@gmail.com>
2022-09-13 22:45:02 +0800

    removed warning

--- src/pages/IDO/index.tsx
+++ src/pages/IDO/index.tsx
@@ -2,37 +2,37 @@ import React from "react";
 import ExploreHeader from "../../components/ExploreHeader";
 
 import { Wrapper } from "./styled";
-enum FILTER_TYPE {
-  LIVE,
-  FINISHED,
-  SCHEDULED,
-  ALL,
-}
+// enum FILTER_TYPE {
+//   LIVE,
+//   FINISHED,
+//   SCHEDULED,
+//   ALL,
+// }
 
-const FilterButtonOptions: {
-  [key in FILTER_TYPE]: {
-    title: string;
-    backgroundColor?: string;
-    color?: string;
-  };
-} = {
-  [FILTER_TYPE.LIVE]: {
-    title: "Live",
-  },
-  [FILTER_TYPE.FINISHED]: {
-    title: "Sold out",
-    backgroundColor: "#C63939",
-    color: "white",
-  },
-  [FILTER_TYPE.SCHEDULED]: {
-    title: "Scheduled",
-    backgroundColor: "#FCFF5C",
-  },
-  [FILTER_TYPE.ALL]: {
-    title: "All",
-    backgroundColor: "white",
-  },
-};
+// const FilterButtonOptions: {
+//   [key in FILTER_TYPE]: {
+//     title: string;
+//     backgroundColor?: string;
+//     color?: string;
+//   };
+// } = {
+//   [FILTER_TYPE.LIVE]: {
+//     title: "Live",
+//   },
+//   [FILTER_TYPE.FINISHED]: {
+//     title: "Sold out",
+//     backgroundColor: "#C63939",
+//     color: "white",
+//   },
+//   [FILTER_TYPE.SCHEDULED]: {
+//     title: "Scheduled",
+//     backgroundColor: "#FCFF5C",
+//   },
+//   [FILTER_TYPE.ALL]: {
+//     title: "All",
+//     backgroundColor: "white",
+//   },
+// };
 
 const IDO: React.FC = () => {
   return (
