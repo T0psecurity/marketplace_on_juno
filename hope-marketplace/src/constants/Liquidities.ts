a7db0092a9480dc66d3fe92a797c41d9eee9bcd2
phillipscheryl <phillipscheryl615@gmail.com>
2023-01-10 22:51:57 +0800

    added new liquidities

--- hope-marketplace/src/constants/Liquidities.ts
+++ hope-marketplace/src/constants/Liquidities.ts
@@ -12,16 +12,42 @@ export const Liquidities: TLiquidity[] = [
 		tokenA: TokenType.HOPERS,
 		tokenB: TokenType.JUNO,
 		contractAddress:
-			"juno150vc84tmw794wa7hx79qmqaray8fkc5zl8se7zfl8pen5ww9mktslgv6gj",
-		stakingAddress:
-			"juno1u9g9saqe6nq803keqja4c6quknp04uc5zge6yu5j8gg45gdfn5jq0ahd05",
+			"juno1g5j9vd76cqt7fsq22ne7jqfkz4v9ptkvh4jknsvwchpj753atwfs942a25",
+		stakingAddress: "",
+	},
+	{
+		tokenA: TokenType.HOPERS,
+		tokenB: TokenType.ATOM,
+		contractAddress:
+			"juno17estllyc6qyq6tlxg9hv4xwcy069fuu0rmytvkw6xskchcq9czxsd5fd98",
+		stakingAddress: "",
+	},
+	{
+		tokenA: TokenType.HOPERS,
+		tokenB: TokenType.AXELAR,
+		contractAddress:
+			"juno1pugg623zsg2xanvuumna6y4ca48t0la7pxgtt96ed55rctea47lsqxu36a",
+		stakingAddress: "",
+	},
+	{
+		tokenA: TokenType.HOPERS,
+		tokenB: TokenType.HOPE,
+		contractAddress:
+			"juno185jw0uh2v9zn8zfhulu2akxxplcd7fjnkvmp84tkjgtadyuxruzsjnexnw",
+		stakingAddress: "",
 	},
 	{
 		tokenA: TokenType.HOPERS,
 		tokenB: TokenType.RAW,
 		contractAddress:
-			"juno1kqphjk9n0es92y09p4urtvpc3saqhfwwau72t84ggyqcz5s5ujyq9ydz2x",
-		stakingAddress:
-			"juno1w9pqw9ej9snm9nxhlk6ekrhstl39c2xvp3r82m93u2zws8h0qyysnqvccv",
+			"juno1qm57tvmvnplv5xym9gu8xjrxmwzv2e85meyhvsax52e5s4phax4s03uvq5",
+		stakingAddress: "",
+	},
+	{
+		tokenA: TokenType.HOPERS,
+		tokenB: TokenType.NETA,
+		contractAddress:
+			"juno19kfk94nqz7ehdmkk5a9hcl8qxq9kksupqzurz9gsgs4tk5xlgqqqrr067a",
+		stakingAddress: "",
 	},
 ];
